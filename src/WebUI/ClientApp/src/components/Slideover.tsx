a87aa9afd47d157e8de8613fb5459964a052f45c
Otis Daley <catalystxninite@gmail.com>
2021-06-17 16:43:44 +0100

    Transaction form and slideover

--- src/WebUI/ClientApp/src/components/Slideover.tsx
+++ src/WebUI/ClientApp/src/components/Slideover.tsx
@@ -1,19 +1,27 @@
-import {Fragment, ReactNode, useState} from 'react'
-import {Dialog, Transition} from '@headlessui/react'
-import {XIcon} from '@heroicons/react/outline'
-
+import {Fragment, ReactNode, useEffect, useState} from "react";
+import {Dialog, Transition} from "@headlessui/react";
+import {XIcon} from "@heroicons/react/outline";
 
 interface IProps {
-
-    children?: ReactNode
+    children?: ReactNode;
+    title?: string;
+    description?: string;
+    isOpen: boolean;
+    handleClose: () => void
+    formName: string
 }
 
-export default function Slideover({children}: IProps) {
-    const [open, setOpen] = useState(true)
+export default function Slideover({children, title, description, isOpen, handleClose, formName}: IProps) {
 
     return (
-        <Transition.Root show={open} as={Fragment}>
-            <Dialog as="div" static className="fixed inset-0 overflow-hidden" open={open} onClose={setOpen}>
+        <Transition.Root show={isOpen} as={Fragment}>
+            <Dialog
+                as="div"
+                static
+                className="fixed inset-0 overflow-hidden"
+                open={isOpen}
+                onClose={handleClose}
+            >
                 <div className="absolute inset-0 overflow-hidden">
                     <Dialog.Overlay className="absolute inset-0"/>
 
@@ -27,18 +35,19 @@ export default function Slideover({children}: IProps) {
                             leaveFrom="translate-x-0"
                             leaveTo="translate-x-full"
                         >
-                            <div className="w-screen max-w-md">
-                                <form className="h-full divide-y divide-gray-200 flex flex-col bg-white shadow-xl">
+                            <div className="w-screen max-w-md pt-16">
+                                <div className="h-full divide-y divide-gray-200 flex flex-col bg-white shadow-xl">
                                     <div className="flex-1 h-0 overflow-y-auto">
-                                        <div className="py-6 px-4 bg-indigo-700 sm:px-6">
+                                        <div className="py-6 px-4 bg-cyan-700 sm:px-6">
                                             <div className="flex items-center justify-between">
-                                                <Dialog.Title className="text-lg font-medium text-white">New
-                                                    Project</Dialog.Title>
+                                                <Dialog.Title className="text-lg font-medium text-white">
+                                                    {title}
+                                                </Dialog.Title>
                                                 <div className="ml-3 h-7 flex items-center">
                                                     <button
                                                         type="button"
-                                                        className="bg-indigo-700 rounded-md text-indigo-200 hover:text-white focus:outline-none focus:ring-2 focus:ring-white"
-                                                        onClick={() => setOpen(false)}
+                                                        className="bg-indigo-700 rounded-md text-cyan-200 hover:text-white focus:outline-none focus:ring-2 focus:ring-white"
+                                                        onClick={handleClose}
                                                     >
                                                         <span className="sr-only">Close panel</span>
                                                         <XIcon className="h-6 w-6" aria-hidden="true"/>
@@ -46,37 +55,33 @@ export default function Slideover({children}: IProps) {
                                                 </div>
                                             </div>
                                             <div className="mt-1">
-                                                <p className="text-sm text-indigo-300">
-                                                    Get started by filling in the information below to create your new
-                                                    project.
-                                                </p>
+                                                <p className="text-sm text-indigo-300">{description}</p>
                                             </div>
                                         </div>
-
                                         {children}
-
                                     </div>
                                     <div className="flex-shrink-0 px-4 py-4 flex justify-end">
                                         <button
                                             type="button"
                                             className="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                                            onClick={() => setOpen(false)}
+                                            onClick={handleClose}
                                         >
                                             Cancel
                                         </button>
                                         <button
+                                            form={formName}
                                             type="submit"
                                             className="ml-4 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                                         >
                                             Save
                                         </button>
                                     </div>
-                                </form>
+                                </div>
                             </div>
                         </Transition.Child>
                     </div>
                 </div>
             </Dialog>
         </Transition.Root>
-    )
+    );
 }
--- src/WebUI/ClientApp/src/pages/Fund.tsx
+++ src/WebUI/ClientApp/src/pages/Fund.tsx
@@ -1,59 +1,124 @@
 import React, {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
 import {fsapi} from "../api/fsapi";
-import {IFundVm} from "../api/web-api-client";
+import {CreateTransactionCommand, IFundVm, TransactionType} from "../api/web-api-client";
 import {ScaleIcon} from "@heroicons/react/solid";
+import Slideover from "../components/Slideover";
+import {object, string, z} from "zod";
+import {useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
+import moment from "moment";
+
+
+//TODO:: form state needs to be change but requires Controller component
+export const transactionFormSchema = object({
+    amount: string(),
+    //TODO:: change type to native enum
+    type       : string(),
+    description: string(),
+    //TODO:: change date to date
+    date: string(),
+});
+
+type transactionSubmission = z.infer<typeof transactionFormSchema>;
 
 
 export function Fund() {
+    const [vm, setVm] = useState<IFundVm>();
+    const [isOpen, setIsOpen] = useState<boolean>(false);
 
-    const [vm, setVm] = useState<IFundVm>()
+    const {
+        register,
+        handleSubmit,
+        formState: {errors},
+    } = useForm({
+        resolver: zodResolver(transactionFormSchema),
+    });
 
-    const {id} = useParams<{ id: string }>();
+    const {id, page} = useParams<{ id: string, page?: string }>();
 
     useEffect(() => {
+
         async function getFund(id: string, page: number = 1, size: number = 10) {
             const client = await fsapi();
 
-            client.funds_GetFund(id, page, size).then(data => setVm(data))
+            client.funds_GetFund(id, page, size).then((data) => setVm(data));
         }
 
-        getFund(id).catch(e => console.error("Fund Page has Error"));
-    }, [id])
+        const currentPage = page ? parseInt(page) : 1
+        getFund(id, currentPage).catch((e) => console.error("Fund Page has Error", e));
+    }, [id, page]);
+
+    async function createTransaction(data: transactionSubmission) {
+        const client = await fsapi();
+        const command = CreateTransactionCommand.fromJS(data)
+        command.type = parseInt(data.type)
+        command.amount = parseInt(data.amount)
+        command.fundId = vm?.fund?.id;
+        client.transactions_CreateTransaction(command).then();
+    }
+
 
     function getCards() {
-        const fund = vm?.fund
+        const fund = vm?.fund;
         const cards = [];
         if (fund) {
-            cards.push({name: 'Account Balance', href: '#', icon: ScaleIcon, amount: '£' + fund.balance},)
-            cards.push({name: 'Account Income', href: '#', icon: ScaleIcon, amount: '£' + fund.income},)
-            cards.push({name: 'Account Expense', href: '#', icon: ScaleIcon, amount: '£' + fund.expenses},)
+            cards.push(
+                {
+                    name  : "Account Balance",
+                    href  : "#",
+                    icon  : ScaleIcon,
+                    amount: "£" + fund.balance,
+                },
+                {
+                    name  : "Account Income",
+                    href  : "#",
+                    icon  : ScaleIcon,
+                    amount: "£" + fund.income,
+                },
+                {
+                    name  : "Account Expense",
+                    href  : "#",
+                    icon  : ScaleIcon,
+                    amount: "£" + fund.expenses,
+                }
+            );
         }
 
-        return cards
+        return cards;
     }
 
     return (
         <div>
-
             <div className="mt-8">
                 <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
-                    <h2 className="text-lg leading-6 font-medium text-gray-900">Overview</h2>
+                    <h2 className="text-lg leading-6 font-medium text-gray-900">
+                        Overview
+                    </h2>
                     <div className="mt-2 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
                         {/* Card */}
                         {getCards().map((card) => (
-                            <div key={card.name} className="bg-white overflow-hidden shadow rounded-lg">
+                            <div
+                                key={card.name}
+                                className="bg-white overflow-hidden shadow rounded-lg"
+                            >
                                 <div className="p-5">
                                     <div className="flex items-center">
                                         <div className="flex-shrink-0">
-                                            <card.icon className="h-6 w-6 text-gray-400" aria-hidden="true"/>
+                                            <card.icon
+                                                className="h-6 w-6 text-gray-400"
+                                                aria-hidden="true"
+                                            />
                                         </div>
                                         <div className="ml-5 w-0 flex-1">
                                             <dl>
-                                                <dt className="text-sm font-medium text-gray-500 truncate">{card.name}</dt>
+                                                <dt className="text-sm font-medium text-gray-500 truncate">
+                                                    {card.name}
+                                                </dt>
                                                 <dd>
-                                                    <div
-                                                        className="text-lg font-medium text-gray-900">{card.amount}</div>
+                                                    <div className="text-lg font-medium text-gray-900">
+                                                        {card.amount}
+                                                    </div>
                                                 </dd>
                                             </dl>
                                         </div>
@@ -61,7 +126,10 @@ export function Fund() {
                                 </div>
                                 <div className="bg-gray-50 px-5 py-3">
                                     <div className="text-sm">
-                                        <a href={card.href} className="font-medium text-cyan-700 hover:text-cyan-900">
+                                        <a
+                                            href={card.href}
+                                            className="font-medium text-cyan-700 hover:text-cyan-900"
+                                        >
                                             View all
                                         </a>
                                     </div>
@@ -72,12 +140,18 @@ export function Fund() {
                 </div>
 
                 {/* Table */}
-
-                <h2 className="max-w-6xl mx-auto mt-8 px-4 text-lg leading-6 font-medium text-gray-900 sm:px-6 lg:px-8">
-                    Transaction Activity
-                </h2>
-
-
+                <div className="max-w-6xl mx-auto flex justify-between mt-8 px-4 sm:px-6 lg:px-8">
+                    <h2 className="text-lg leading-6 font-medium text-gray-900 ">
+                        Transaction Activity
+                    </h2>
+                    <button
+                        onClick={() => setIsOpen(true)}
+                        type="button"
+                        className="items-center px-2.5 py-1.5 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-cyan-600 hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500"
+                    >
+                        Create Transaction
+                    </button>
+                </div>
 
                 <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 mt-4">
                     <div className="flex flex-col">
@@ -117,20 +191,39 @@ export function Fund() {
                                         </tr>
                                         </thead>
                                         <tbody>
-                                        {vm?.transactions?.items?.map((transaction, transactionIdx) => (
-                                            <tr key={transaction.id}
-                                                className={transactionIdx % 2 === 0 ? 'bg-white' : 'bg-gray-50'}>
-                                                <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{transaction.date}</td>
-                                                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{transaction.amount}</td>
-                                                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{transaction.type}</td>
-                                                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{transaction.description}</td>
-                                                <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                                    <a href="#" className="text-indigo-600 hover:text-indigo-900">
-                                                        Edit
-                                                    </a>
-                                                </td>
-                                            </tr>
-                                        ))}
+                                        {vm?.transactions?.items?.map(
+                                            (transaction, transactionIdx) => (
+                                                <tr
+                                                    key={transaction.id}
+                                                    className={
+                                                        transactionIdx % 2 === 0
+                                                            ? "bg-white"
+                                                            : "bg-gray-50"
+                                                    }
+                                                >
+                                                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                                                        {moment(transaction.date).format('DD/MM/YYYY')}
+                                                    </td>
+                                                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                                        {"£" + transaction.amount}
+                                                    </td>
+                                                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                                        {transaction.type}
+                                                    </td>
+                                                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                                        {transaction.description}
+                                                    </td>
+                                                    <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                                        <a
+                                                            href="#"
+                                                            className="text-indigo-600 hover:text-indigo-900"
+                                                        >
+                                                            Edit
+                                                        </a>
+                                                    </td>
+                                                </tr>
+                                            )
+                                        )}
                                         </tbody>
                                     </table>
                                 </div>
@@ -140,6 +233,72 @@ export function Fund() {
                 </div>
             </div>
 
+            <Slideover title="New Transaction" isOpen={isOpen} handleClose={() => setIsOpen(false)} formName="createTransaction">
+                <div className="px-3 py-2">
+                    <form id="createTransaction" onSubmit={handleSubmit(createTransaction)}>
+                        <label
+                            htmlFor="amount"
+                            className="block text-sm font-medium text-gray-700"
+                        >
+                            Amount
+                        </label>
+                        <input
+                            {...register("amount")}
+                            type="text"
+                            id="price"
+                            className="focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                            placeholder="0.00"
+                            aria-describedby="transaction-amount"
+                        />
+                        <p>{errors.amount?.message}</p>
+                        <label
+                            htmlFor="description"
+                            className="block text-sm font-medium text-gray-700"
+                        >
+                            Description
+                        </label>
+                        <input
+                            {...register("description")}
+                            type="text"
+                            id="description"
+                            className="focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                            aria-describedby="transaction-description"
+                        />
+                        <p>{errors.description?.message}</p>
+
+                        <label
+                            htmlFor="date"
+                            className="block text-sm font-medium text-gray-700"
+                        >
+                            Date
+                        </label>
+                        <input
+                            {...register("date")}
+                            type="date"
+                            id="date"
+                            className="focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                            aria-describedby="transaction-date"
+                        />
+                        <p>{errors.date?.message}</p>
+                        <label
+                            htmlFor="type"
+                            className="block text-sm font-medium text-gray-700"
+                        >
+                            Type
+                        </label>
+                        <select
+                            {...register("type")}
+                            id="type"
+                            className="focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                            aria-describedby="transaction type"
+                        >
+                            <option value={TransactionType.Income}>Income</option>
+                            <option value={TransactionType.Expense}>Expense</option>
+                        </select>
+                        <p>{errors.type?.message}</p>
+                    </form>
+                </div>
+            </Slideover>
         </div>
-    )
+    );
 }
