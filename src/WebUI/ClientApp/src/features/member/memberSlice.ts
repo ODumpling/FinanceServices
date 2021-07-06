79e3b19b921fafb0116818b03582b1d830f90db0
Otis Daley <catalystxninite@gmail.com>
2021-07-06 17:22:54 +0100

    added Members to store

---
+++ src/WebUI/ClientApp/src/features/member/memberSlice.ts
@@ -0,0 +1,34 @@
+﻿import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {IMemberDto} from "../../api/web-api-client";
+import {AppDispatch} from "../../store/store";
+import {financeApi} from "../../api/financeApi";
+
+interface IState {
+    allMembers: IMemberDto[]
+}
+
+const initialState: IState = {
+    allMembers: []
+}
+export const memberSlice = createSlice({
+    name: "members",
+    initialState,
+    reducers: {
+        setMembers: function (state, action: PayloadAction<IMemberDto[]>) {
+            state.allMembers = action.payload;
+        }
+    }
+});
+
+export default memberSlice.reducer;
+
+export const {setMembers} = memberSlice.actions;
+
+export const getAllMembers = () => async (dispatch: AppDispatch) => {
+    try {
+        const response = await (await financeApi()).memberships_GetMembers(null);
+        dispatch(setMembers(response))
+    } catch (e) {
+        return console.error(e.message);
+    }
+}
--- src/WebUI/ClientApp/src/store/store.ts
+++ src/WebUI/ClientApp/src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import fundReducer from "../features/fund/fundsSlice";
+import memberReducer from "../features/member/memberSlice";
 
 export const store = configureStore({
   reducer: {
     funds: fundReducer,
+    members: memberReducer,
   },
 });
 
