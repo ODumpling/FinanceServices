9f9bac20e6a164a89d7c7c5b23c6b103388a619b
Otis Daley <catalystxninite@gmail.com>
2021-07-06 17:25:55 +0100

    reworked how homepage and components work

--- src/WebUI/ClientApp/src/pages/Home.tsx
+++ src/WebUI/ClientApp/src/pages/Home.tsx
@@ -1,29 +1,27 @@
-import React, {Fragment, useEffect, useState} from "react";
-import {useHistory} from "react-router-dom";
-import {useQuery} from "../hooks/useQuery";
-import {useForm} from "react-hook-form";
-import {zodResolver} from "@hookform/resolvers/zod";
-import {object, string, z} from "zod";
-import {useAppDispatch, useAppSelector} from "../hooks";
-import {fetchFunds} from "../features/fund/fundsSlice";
-import {FundsTable} from "../features/fund/FundsTable";
-
-export const fundFormSchema = object({
-    name: string(),
-});
-
-type fundSubmission = z.infer<typeof fundFormSchema>;
+import React, { Fragment, useEffect } from "react";
+import { FundsTable } from "../features/fund/FundsTable";
+import { useAppDispatch, useAppSelector } from "../hooks";
+import { getFunds } from "../features/fund/fundsSlice";
 
 export function Home() {
-    
+  const dispatch = useAppDispatch();
+  const { listOfFunds: funds } = useAppSelector((state) => state.funds);
 
+  useEffect(() => {
+    dispatch(getFunds(1, 10));
+  }, [dispatch]);
 
-
-
-    return (
-        <Fragment>
-            <FundsTable currentPage={(data) => console.log(data)}/>
-
-        </Fragment>
-    );
+  return (
+    <Fragment>
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 mt-5">
+        <h1 className="text-lg leading-6 font-medium text-gray-900">
+          Fund List
+        </h1>
+      </div>
+      <FundsTable
+        funds={funds}
+        currentPage={(page) => dispatch(getFunds(page, 10))}
+      />
+    </Fragment>
+  );
 }
